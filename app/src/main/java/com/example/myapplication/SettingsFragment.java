ce1ca3fee98f7d2909881d90a4b6280c318a4e07
BNSC1 <19542357+BNSC1@users.noreply.github.com>
2020-08-06 13:27:18 +0800

    Updated settings.

--- app/src/main/java/com/example/myapplication/SettingsFragment.java
+++ app/src/main/java/com/example/myapplication/SettingsFragment.java
@@ -18,8 +18,6 @@ import androidx.fragment.app.Fragment;
  * A simple {@link Fragment} subclass.
  */
 public class SettingsFragment extends Fragment{
-    SharedPreferences settings;
-    SharedPreferences.Editor editor;
     EditText pomodoroLength, breakLength, restLength;
     Spinner alertType;
 
@@ -33,18 +31,25 @@ public class SettingsFragment extends Fragment{
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextChangedListener tc=new TextChangedListener(this.getActivity());
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        settings=getActivity().getSharedPreferences("Pomodoro Length", Context.MODE_PRIVATE);
+//        settings=getActivity().getSharedPreferences("Pomodoro Length", Context.MODE_PRIVATE);
+
         return inflater.inflate(R.layout.fragment_settings, container, false);
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        pomodoroLength = getView().findViewById(R.id.pmET);
-        breakLength =getView().findViewById(R.id.brET);
-        restLength =getView().findViewById(R.id.rsET);
-        alertType=getView().findViewById(R.id.alertSP);
+        pomodoroLength =(EditText) getView().findViewById(R.id.pmET);
+        breakLength =(EditText) getView().findViewById(R.id.brET);
+        restLength =(EditText) getView().findViewById(R.id.rsET);
+        alertType=(Spinner) getView().findViewById(R.id.alertSP);
     }
 }
--- app/src/main/java/com/example/myapplication/TextChangedListener.kt
+++ app/src/main/java/com/example/myapplication/TextChangedListener.kt
@@ -1,4 +1,36 @@
 package com.example.myapplication
 
-class TextChangedListener {
+import android.content.Context
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+class TextChangedListener(fragmentActivity: FragmentActivity) : TextWatcher {
+    var view:View?=null
+    var settings: SharedPreferences? = null
+    var editor: Editor? = null
+    var mContext:FragmentActivity?=null
+    fun TextChangedListener(view:View){
+        this.view=view
+    }
+    fun TextChangedListener(context: FragmentActivity){
+        this.mContext=context
+    }
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // Inflate the layout for this fragment
+        settings = mContext?.getSharedPreferences("Pomodoro Length", Context.MODE_PRIVATE)
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+
+    }
 }
+public class Util{}
